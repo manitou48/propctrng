6e0ffcb5f79dc41df9cdb7ba776e98149ffe31bc
dunigan <dunigan@thistle.(none)>
2012-08-19 16:53:44 -0400

    add stats on randomness

--- propctrng.c
+++ propctrng.c
@@ -15,6 +15,17 @@
 #include <stdio.h>
 #include <propeller.h>                    // propeller specific definitions
 
+// http://gurmeetsingh.wordpress.com/2008/08/05/fast-bit-counting-routines/
+#define MASK_01010101 (((unsigned int)(-1))/3)
+#define MASK_00110011 (((unsigned int)(-1))/5)
+#define MASK_00001111 (((unsigned int)(-1))/17)
+int bitcount (unsigned int n) {
+	n = (n & MASK_01010101) + ((n >> 1) & MASK_01010101) ;
+	n = (n & MASK_00110011) + ((n >> 2) & MASK_00110011) ;
+	n = (n & MASK_00001111) + ((n >> 4) & MASK_00001111) ;
+	return n % 255 ;
+}
+
 // the STATIC HUB mailbox for communication to PASM routine 
 //
 static unsigned int rng; // a pointer to this gets passed to the PASM code as the PAR register
@@ -36,14 +47,35 @@ int start(unsigned int *pinptr)
 // LMM model
 void main (int argc,  char* argv[])
 {
+	unsigned int cnt=0, prev, current, ones, onescnt=0;
+	static unsigned int bytes[256];
+
     printf("hello, world!\n");            // let the lead LMM COG say hello
     printf ("New COG# %d started.\n",start(&rng)); // start a new COG passing a pointer to the STATIC mailbox structure
     waitcnt(CLKFREQ/200 + CNT);  // give TRNG 5ms to collect entropy
+	prev = rng;
     while (1)
     {
       waitcnt(16000 + CNT);  // TRNG needs 200us for 32 bits, 16000 ticks at 80MHz
-      waitcnt(CLKFREQ + CNT);  // print delay a second
-	  printf("%08x\n",rng);
+	  current = rng;
+	  prev = prev ^ current;
+	  ones = bitcount(prev);
+	  onescnt += ones;
+	  bytes[current &0xff]++;
+	  bytes[(current>>8) &0xff]++;
+	  bytes[(current>>16) &0xff]++;
+	  bytes[(current>>24) &0xff]++;
+	  cnt++;
+      if(cnt % 1000 == 0) {
+		unsigned int i, min = 0xffffffff,mini,max=0,maxi;
+		for (i=0; i< 256;i++) {
+			if (bytes[i] < min) { mini = i; min = bytes[i];}
+			if (bytes[i] > max) { maxi = i; max = bytes[i];}
+		}
+	  	printf("%08x ones %d %d cnt %d  %d min %02x %d max %02x %d \n",
+		 current,ones,onescnt/cnt,cnt, cnt/64,mini,min,maxi,max);
+	  }
+	  prev = current;
     }    
 }
 
